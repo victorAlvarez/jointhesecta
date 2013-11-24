57202bfa1c9c0be3c92d7dd3420dfa15f0a3e755
victorAlvarez <victor87eg6@gmail.com>
2013-11-24 04:08:05 -0800

    services mensajes

--- public/js/services/mensajes.js
+++ public/js/services/mensajes.js
@@ -1,10 +1,15 @@
 //Mensaje service used for mensajes REST endpoint
-angular.module('mean.mensajes').factory("Mensaje", ['$resource', function($resource) {
-    return $resource('mensajes/:mensajeId', {
-        mensajeId: '@_id'
-    }, {
-        update: {
-            method: 'PUT'
-        }
-    });
-}]);
+
+window.angular.module('jst.services.mensajes', [])
+    .factory('Mensaje', ['$resource',
+        function ($resource) {
+            return $resource(
+                'mensajes/:mensajeId',
+                {
+                    mensajeId: '@_id'
+                },
+                {
+                    update: {method: 'PUT'}
+                }
+            );
+        }]);
